9ecd6497f1d27deae544e079725968c16abb1da5
SimasDei <simondeiman@gmail.com>
2019-05-04 20:38:21 +0300

    switch to dark theme, add vuetify toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,25 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+  <v-app dark>
+    <v-toolbar>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>View Meetups
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <main></main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: "App",
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
