155882570a77342f4386e7af36a2216da4c16fdc
SimasDei <simondeiman@gmail.com>
2019-05-05 14:36:39 +0300

    add side drawer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <v-app dark>
     <v-toolbar>
+      <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-xs-only">
         <v-btn flat>
           <v-icon left>supervisor_account</v-icon>View Meetups
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <main></main>
   </v-app>
 </template>
@@ -18,7 +29,7 @@ export default {
   name: "App",
   data() {
     return {
-      //
+      sideNav: false
     };
   }
 };
