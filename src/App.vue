5bc5a4cdfb24d52d590b09e84cbd8495733e6296
SimasDei <simondeiman@gmail.com>
2019-05-05 14:58:50 +0300

    add menu items array, use v-loop on navbar

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,20 @@
       <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
+      <v-toolbar-items class="hidden-xs-only" v-for="item in menuItems" :key="item.title">
         <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>View Meetups
+          <v-icon left>{{item.icon}}</v-icon>
+          {{item.title}}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-navigation-drawer v-model="sideNav">
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>View Meetups</v-list-tile-content>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
@@ -29,7 +30,14 @@ export default {
   name: "App",
   data() {
     return {
-      sideNav: false
+      sideNav: false,
+      menuItems: [
+        { icon: "supervisor_account", title: "View Meetups" },
+        { icon: "room", title: "Organize Meetup" },
+        { icon: "person", title: "Profile" },
+        { icon: "face", title: "Sign up" },
+        { icon: "lock_open", title: "Sign in" }
+      ]
     };
   }
 };
