c803d32989246a7154aadb82e03a0049de20c4c7
Simas <simonas@digitouch.lt>
2019-05-06 17:29:54 +0300

    add Firebase

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -1,8 +1,79 @@
 <template>
-  <div>
-    <p>Sign up Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form @submit.prevent>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="Mail"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      type="password"
+                      :rules="[comparePasswords]"
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit">Sign Up</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+  },
+  computed: {
+    comparePasswords() {
+      return this.password !== this.confirmPassword
+        ? "Passwords do not match"
+        : "";
+    }
+  },
+  methods: {
+    onSubmit() {
+      /**
+       * @todo - VUEX
+       */
+      console.log({
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      });
+    }
+  }
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import * as Firebase from 'firebase';
 import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
@@ -13,4 +14,15 @@ new Vue({
   router,
   store,
   render: h => h(App),
+  created() {
+    Firebase.initializeApp({
+      apiKey: 'AIzaSyBS_v10OY91GZU2m9XCvvZFnZR25oWuc78',
+      authDomain: 'vue-seven.firebaseapp.com',
+      databaseURL: 'https://vue-seven.firebaseio.com',
+      projectId: 'vue-seven',
+      storageBucket: 'vue-seven.appspot.com',
+      messagingSenderId: '189767313248',
+      appId: '1:189767313248:web:a0570087f1b093fd',
+    });
+  },
 }).$mount('#app');
